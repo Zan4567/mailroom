436dc16f158d8706841654ccfdc22ba8007a1aad
Loaye <jr.carstens00@gmail.com>
2017-10-23 15:25:26 -0700

    fixes user input errors, ensures dict keys are returned

--- src/mailroom.py
+++ src/mailroom.py
@@ -1,17 +1,15 @@
 '''.'''
 
-DONOR_DATA = [
-    {'name', [100, 500, 120]}
-]
+DONOR_DATA = {
+    'name': [100, 500, 120]
+}
 
 def prompt():
+    """Prompts a user to make selections"""
     while True:
-        print("""Welcome! Please select:
+        user_input = input("""Welcome! Please select:
             Send a (T)hank You
-            Create a (R)eport""")
-
-        user_input = raw_input()
-
+            Create a (R)eport: """)
         if user_input.startswith(('t', 'T')):
             thankyou()
         elif user_input.startswith(('r', 'R')):
@@ -20,28 +18,26 @@ def prompt():
             break;
 
 def thankyou():
+    """Prompts user for list of donors"""
     name = ''
     while(name == ''):
-        print('Please enter the name of the recipient:')
-        user_input = raw_input()
-
+        print('Please enter the name of the recipient or type list to see a list of donors - or q to quit')
+        user_input = input()
         if user_input == 'list':
-            for donor in DONOR_DATA:
+            for donor in DONOR_DATA.keys():
                 print(donor)
         elif user_input == 'q' or user_input == 'Q':
             print('Exiting to main menu...')
             return
-        elif DONOR_DATA[user_input]:
+        elif DONOR_DATA.get(user_input, False):
             name = user_input
         elif user_input != '':
-            DONOR_DATA.append({user_input}, [])
+            DONOR_DATA.update((user_input, []))
             name = user_input
-
     amount = 0
     while(amount == 0):
         print("Please enter the donation amount:")
-        user_input = raw_input()
-
+        user_input = input()
         try:
             amount = user_input
         except ValueError:
